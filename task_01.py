de82911e738e5c66ccbab51ed6e54d952bc22872
Vagrant User <mr.nikovaccaro@gmail.com>
2015-10-08 02:29:27 +0000

    transferring to other pc

---
+++ task_01.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""..................."""
+
+
+def fibonacci(maxtint):
+    """
+    This function 
+
+    Args:
+        maxint (int):
+
+    Returns:
+        int: 
+
+    Examples:
+
+    """
+    lastnum, curnum = curnum, lastnum + curnum
+    
